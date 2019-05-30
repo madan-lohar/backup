ca3512bb0c92d863ba65cfcddb6850e01ab65d79
MadanLohr <50138147+MadanLohr@users.noreply.github.com>
2019-05-30 12:43:38 +0530

    initial

---
+++ rodLaver/RodLaverArenaAccessibility.js
@@ -0,0 +1,299 @@
+var rodLaverArenaAccessibility = [{
+    "id": 22,
+    "seat": "U - 10",
+    "isCompanion": false
+}, {
+    "id": 2,
+    "seat": "U - 11",
+    "isCompanion": true
+}, {
+    "id": 3,
+    "seat": "U - 12",
+    "isCompanion": false
+}, {
+    "id": 4,
+    "seat": "U - 13",
+    "isCompanion": true
+}, {
+    "id": 5,
+    "seat": "U - 14",
+    "isCompanion": false
+}, {
+    "id": 6,
+    "seat": "U - 15",
+    "isCompanion": true
+}, {
+    "id": 7,
+    "seat": "U - 16",
+    "isCompanion": false
+}, {
+    "id": 8,
+    "seat": "U - 17",
+    "isCompanion": true
+}, {
+    "id": 9,
+    "seat": "U - 18",
+    "isCompanion": false
+}, {
+    "id": 10,
+    "seat": "U - 19",
+    "isCompanion": true
+}, {
+    "id": 11,
+    "seat": "U - 20",
+    "isCompanion": false
+}, {
+    "id": 12,
+    "seat": "U - 21",
+    "isCompanion": true
+}, {
+    "id": 13,
+    "seat": "U - 22",
+    "isCompanion": false
+}, {
+    "id": 14,
+    "seat": "U - 23",
+    "isCompanion": true
+}, {
+    "id": 15,
+    "seat": "U - 24",
+    "isCompanion": false
+}, {
+    "id": 16,
+    "seat": "U - 25",
+    "isCompanion": true
+}, {
+    "id": 17,
+    "seat": "U - 26",
+    "isCompanion": false
+}, {
+    "id": 18,
+    "seat": "U - 27",
+    "isCompanion": true
+}, {
+    "id": 19,
+    "seat": "U - 28",
+    "isCompanion": false
+}, {
+    "id": 20,
+    "seat": "U - 29",
+    "isCompanion": true
+},
+{
+    "id": 21,
+    "seat": "U - 30",
+    "isCompanion": false
+},
+
+{
+    "id": 1,
+    "seat": "L - 4",
+    "isCompanion": false
+}, {
+    "id": 23,
+    "seat": "L - 5",
+    "isCompanion": true
+}, {
+    "id": 24,
+    "seat": "L - 6",
+    "isCompanion": false
+}, {
+    "id": 25,
+    "seat": "L - 7",
+    "isCompanion": true
+}, {
+    "id": 26,
+    "seat": "L - 8",
+    "isCompanion": false
+}, {
+    "id": 27,
+    "seat": "L - 9",
+    "isCompanion": true
+}, {
+    "id": 28,
+    "seat": "L - 10",
+    "isCompanion": false
+}, {
+    "id": 29,
+    "seat": "L - 11",
+    "isCompanion": true
+}, {
+    "id": 30,
+    "seat": "L - 12",
+    "isCompanion": false
+}, {
+    "id": 31,
+    "seat": "L - 13",
+    "isCompanion": true
+},
+
+{
+    "id": 32,
+    "seat": "G - 10",
+    "isCompanion": false
+}, {
+    "id": 33,
+    "seat": "G - 11",
+    "isCompanion": true
+}, {
+    "id": 34,
+    "seat": "G - 12",
+    "isCompanion": false
+}, {
+    "id": 35,
+    "seat": "G - 13",
+    "isCompanion": true
+}, {
+    "id": 36,
+    "seat": "G - 14",
+    "isCompanion": false
+}, {
+    "id": 37,
+    "seat": "G - 15",
+    "isCompanion": true
+}, {
+    "id": 38,
+    "seat": "G - 16",
+    "isCompanion": false
+}, {
+    "id": 39,
+    "seat": "G - 17",
+    "isCompanion": true
+}, {
+    "id": 40,
+    "seat": "G - 18",
+    "isCompanion": false
+}, {
+    "id": 41,
+    "seat": "G - 19",
+    "isCompanion": true
+},
+
+{
+    "id": 42,
+    "seat": "D - 8",
+    "isCompanion": false
+}, {
+    "id": 43,
+    "seat": "D - 9",
+    "isCompanion": true
+}, {
+    "id": 44,
+    "seat": "D - 10",
+    "isCompanion": false
+}, {
+    "id": 45,
+    "seat": "D - 11",
+    "isCompanion": true
+}, {
+    "id": 46,
+    "seat": "D - 12",
+    "isCompanion": false
+}, {
+    "id": 47,
+    "seat": "D - 13",
+    "isCompanion": true
+}, {
+    "id": 48,
+    "seat": "D - 14",
+    "isCompanion": false
+}, {
+    "id": 49,
+    "seat": "D - 15",
+    "isCompanion": true
+}, {
+    "id": 50,
+    "seat": "D - 16",
+    "isCompanion": false
+}, {
+    "id": 51,
+    "seat": "D - 17",
+    "isCompanion": true
+},
+
+{
+    "id": 52,
+    "seat": "P - 14",
+    "isCompanion": false
+}, {
+    "id": 53,
+    "seat": "P - 15",
+    "isCompanion": true
+}, {
+    "id": 54,
+    "seat": "P - 16",
+    "isCompanion": false
+}, {
+    "id": 55,
+    "seat": "P - 17",
+    "isCompanion": true
+}, {
+    "id": 56,
+    "seat": "P - 18",
+    "isCompanion": false
+}, {
+    "id": 57,
+    "seat": "P - 19",
+    "isCompanion": true
+}, {
+    "id": 58,
+    "seat": "P - 20",
+    "isCompanion": false
+}, {
+    "id": 59,
+    "seat": "P - 21",
+    "isCompanion": true
+}, {
+    "id": 60,
+    "seat": "P - 22",
+    "isCompanion": false
+}, {
+    "id": 61,
+    "seat": "P - 23",
+    "isCompanion": true
+},
+
+{
+    "id": 62,
+    "seat": "B - 3",
+    "isCompanion": false
+}, {
+    "id": 63,
+    "seat": "B - 4",
+    "isCompanion": true
+}, {
+    "id": 64,
+    "seat": "B - 5",
+    "isCompanion": false
+}, {
+    "id": 65,
+    "seat": "B - 6",
+    "isCompanion": true
+}, {
+    "id": 66,
+    "seat": "B - 7",
+    "isCompanion": false
+}, {
+    "id": 67,
+    "seat": "B - 8",
+    "isCompanion": true
+}, {
+    "id": 68,
+    "seat": "B - 9",
+    "isCompanion": false
+}, {
+    "id": 69,
+    "seat": "B - 10",
+    "isCompanion": true
+}, {
+    "id": 70,
+    "seat": "B - 11",
+    "isCompanion": false
+}, {
+    "id": 71,
+    "seat": "B - 12",
+    "isCompanion": true
+}
+
+];
+export default rodLaverArenaAccessibility;
